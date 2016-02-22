f6189cf3938567ce0bfdb73c306bf2382b5ef51c
mcpeleeb <20jumpsgame@gmail.com>
2016-02-22 20:04:33 +0000

    Create splashtext.js

---
+++ splashtext.js
@@ -0,0 +1,29 @@
+username = app.ReadFile( directory + "/score.txt" )
+
+var splash = ["2000 Jumps !","Undefeated","Got Your Nose", "Shoutout To Thejaustin","shoutout To " +  username, "Random Splash", "Coding 420 Swag", "Brofist", "JOHHHNN CEnnna !", "Batman", "BUtttter","You used to call me on my " + model,"More splashes then A Swimming Pool", "VERIFY @MCPELeeB√ :/", "Say my name","NOTICED ME", "Jump 2.0 SUPER CONFIRMED", "Powered by electricity", "∆ Confirmed", "Christmas Dlc","XD", "`_`", "At This Moment" + username + "Knew He Messed up", "Edited In Paint", "10/10 Ign", "Freeeeeeeeeeeeee", " What Are Thoooooooooooose","Much Blocks Wow", "FPS :/", "as Seen On Tv", "MYSPACE.....---___--", "Deez Blocks", "Followed By @verified","Eat Sleep Eat Repeat","Running Through The six", "Ayyyyy", "MEMES INBOUND", "Justin Bieber $", "9+10?", "Vines And Tweets ;/", "straight outta COMPTON", " Ariana grande '___'", "Password: Password123", "DONT DIG DOWN","10 Downloads :", " Rate 5 Stars", "Hit The quan", " MEeeeeeoooow", "Hit The Nae Nae", "@Mcpeleeb A God" ,"Oh My", "Never Let me go Jack", "Its All Ogre Now", "This is My Swamp"," Get rekkkkktt"," Half Life 3 Confirmed?", "This is a Lonnnnnnnnnng Message XD"," Y The Hell You Lying"," 9+10= Banter"," What Are Thossssseee!" , " Your a wizard Harryy", "Hola At Me"," IM A Duck"," Mcpeleeb IS Bae"," the real slim shady"," Shes My Trap queen", "I Am A Potato"," tactical Nuke Incoming"," 20 Jumps.Exe ?", "lol","Its Just A Prank Bro", "Colorrrrrrrrr", " Merry Christmas", "Christmas Update!", "Drake!!", "Ever since i left the city", "I Know When That Hotline Bling ", "Rudolph The Reindeer !!", "Donald Bump !!!", " Dlc Out now $1999 For Hacks", "2 Jumps","Laggier than a picture", "Eyebrows On Point", "All I Want For Christmas Is 20 Jumps", "20 Jumps Pc?", "Powered By Air", "Version 0.0", "Shout out to @Turkeysniper"," My Space Verified", "20 Jumps And Chill....", " Black Friday ---_____----", "Hit The Nae Nae", "I Like Turtles", "Timmmmmmy", "Wanna Hear A Joke.. Ios", "Awkwardddd", "Were My baes At??","Another One", "And Another One", "Lionnnnnn","say WhatttT","Render","Ayyy", "Lmao You Have " + score.deaths + " Deaths XD", "in Development", "This Is a Message XD","That Changelog Tho","Pronounced 20 Jumps!","As Seen On Tv","Limited Edition","Error 404!!!!!!!","Shoutout to @Raskotweetz","95% Bug Free","Indie!!!","Now In Amazing color!","Cooler than Spock!","0% sugar!","Supercalifragilisticexpialidocious!","Jump up, jump up, and get down!","20/20","Do not disturb","Plan is fullproof","PRISON BREAK : D ", "Explosions!!!!","Inspired by bob","Be like bill","Now Including squares!!!","Y U read this","More random than a array","Tell yo freinds","Spread the word","404 Game not found","This is my true form","Booooooo","Tested","95% Bugs","1/2","#IT" ]; 
+
+var levelTexts = {
+    
+    0:  "Press Back To Skip Levels, Press Blue Icon for Menu",
+    1:  splash[Math.floor(Math.random()*splash.length)],
+    2:  splash[Math.floor(Math.random()*splash.length)],
+    3:  splash[Math.floor(Math.random()*splash.length)],
+    4:  splash[Math.floor(Math.random()*splash.length)],
+    5:  splash[Math.floor(Math.random()*splash.length)],
+    6:  splash[Math.floor(Math.random()*splash.length)],
+    7:  splash[Math.floor(Math.random()*splash.length)],
+    8:  splash[Math.floor(Math.random()*splash.length)],
+    9: splash[Math.floor(Math.random()*splash.length)],
+    10: splash[Math.floor(Math.random()*splash.length)],
+    11:  splash[Math.floor(Math.random()*splash.length)],
+    12: splash[Math.floor(Math.random()*splash.length)],
+    13: splash[Math.floor(Math.random()*splash.length)],
+    14: splash[Math.floor(Math.random()*splash.length)],
+    15: splash[Math.floor(Math.random()*splash.length)],
+    16: splash[Math.floor(Math.random()*splash.length)],
+    17: splash[Math.floor(Math.random()*splash.length)],
+    18: splash[Math.floor(Math.random()*splash.length)],
+    19: splash[Math.floor(Math.random()*splash.length)],
+    20: splash[Math.floor(Math.random()*splash.length)],
+    21: splash[Math.floor(Math.random()*splash.length)]
+}
